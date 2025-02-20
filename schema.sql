2867c00db132af42d4a70e2c548b1d0688eab352
LashkaPashka <106153640+LashkaPashka@users.noreply.github.com>
2025-02-20 18:25:47 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,15 @@
+-- Table for Postgres
 CREATE TABLE links (
-	id SERIAL PRIMARY KEY, -- первичный ключ
+    id SERIAL PRIMARY KEY, -- первичный ключ
     url VARCHAR(20) NOT NULL DEFAULT '',
     hash VARCHAR(20) NOT NULL DEFAULT '',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+	
+-- Table for mySQL --
+CREATE TABLE stats (
+    id SERIAL PRIMARY KEY, -- первичный ключ
+    url VARCHAR(20) NOT NULL DEFAULT '',
+    click INTEGER NOT NULL DEFAULT '',
+    average_length INTEGER DEFAULT CURRENT_TIMESTAMP
 )
